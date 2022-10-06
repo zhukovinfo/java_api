7512fb5fbcd5d178d54cc9094680759ce1dd8860
Andrey Zhukov <zhukovinfo@gmail.com>
2022-10-06 22:11:02 +0400

    Added HelloWorldTest

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,10 +1,20 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.Test;
 
 public class HelloWorldTest {
 
   @Test
+  public void testHello() {
+    System.out.println("Hello from Andrey Zhukov");
+  }
+
+  @Test
   public void testHelloWorld() {
-    System.out.println("Hello, world");
+    Response response = RestAssured
+        .get("https://playground.learnqa.ru/api/hello")
+        .andReturn();
+    response.prettyPrint();
   }
 
 }
