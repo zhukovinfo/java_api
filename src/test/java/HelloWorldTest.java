1f4c9c902ff4fbdb24d222a029e0d1677acb8e94
Andrey Zhukov <zhukovinfo@gmail.com>
2022-10-22 00:28:17 +0400

    Added exercise 13

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -215,4 +215,56 @@ public class HelloWorldTest {
         "Header is not homework: hw_value");
   }
 
+  @ParameterizedTest
+  @ValueSource(strings = {
+      "Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) "
+          + "Version/4.0 Mobile Safari/534.30",
+      "Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 "
+          + "Mobile/15E148 Safari/604.1",
+      "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 "
+          + "Safari/537.36 Edg/91.0.100.0",
+      "Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 "
+          + "Mobile/15E148 Safari/604"
+  })
+  public void userAgentTest(String userAgentRequest) {
+    JsonPath response = RestAssured
+        .given()
+        .header("User-Agent", userAgentRequest)
+        .get("https://playground.learnqa.ru/ajax/api/user_agent_check")
+        .jsonPath();
+
+    String userAgentResponse = response.getString("user_agent");
+    String platform = response.getString("platform");
+    String browser = response.getString("browser");
+    String device = response.getString("device");
+
+    if (Objects.equals(userAgentRequest,
+        "Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 "
+            + "(KHTML, like Gecko) Version/4.0 Mobile Safari/534.30")) {
+      assertEquals(userAgentRequest, userAgentResponse);
+      assertEquals("Mobile", platform);
+      assertEquals("No", browser);
+      assertEquals("Android", device);
+    } else if (Objects.equals(userAgentRequest,
+        "Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 "
+            + "Mobile/15E148 Safari/604.1")) {
+      assertEquals(userAgentRequest, userAgentResponse);
+      assertEquals("Mobile", platform);
+      assertEquals("Chrome", browser);
+      assertEquals("iOS", device);
+    } else if (Objects.equals(userAgentRequest, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+        + "(KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.")) {
+      assertEquals(userAgentRequest, userAgentResponse);
+      assertEquals("Web", platform);
+      assertEquals("Chrome", browser);
+      assertEquals("No", device);
+    } else if (Objects.equals(userAgentRequest, "Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) "
+        + "AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1")) {
+      assertEquals(userAgentRequest, userAgentResponse);
+      assertEquals("Mobile", platform);
+      assertEquals("No", browser);
+      assertEquals("iPhone", device);
+    }
+  }
 }
