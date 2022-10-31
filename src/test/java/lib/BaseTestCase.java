95c6661027e731eee38fa2541d64854db7ece7d2
Andrey Zhukov <zhukovinfo@gmail.com>
2022-10-31 22:13:42 +0400

    small improvement Base class

--- src/test/java/lib/BaseTestCase.java
+++ src/test/java/lib/BaseTestCase.java
@@ -10,11 +10,10 @@ import java.util.Map;
 
 public class BaseTestCase {
 
-  public static final String HOST_URL = "https://playground.learnqa.ru/";
-  public static final String USER_URL = HOST_URL + "api/user/";
-  public static final String LOGIN_URL = HOST_URL + "api/user/login";
-
-  public static final String AUTH_URL = HOST_URL + "api/user/auth";
+  private static final String HOST = "https://playground.learnqa.ru/";
+  public static final String USER_URL = HOST + "api/user/";
+  public static final String LOGIN_URL = HOST + "api/user/login";
+  public static final String AUTH_URL = HOST + "api/user/auth";
   public ApiCoreRequests apiCoreRequests =  new ApiCoreRequests();
 
   protected String getHeader(Response response, String name) {
